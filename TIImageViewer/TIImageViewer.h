2921c4c8123f30458c0c75cece48e41f389f79b5
Todd Isaacs <teisaacs21w@gmail.com>
2017-01-01 11:34:21 -0500

    Initial Commit

---
+++ TIImageViewer/TIImageViewer.h
@@ -0,0 +1,19 @@
+//
+//  TIImageViewer.h
+//  TIImageViewer
+//
+//  Created by Todd Isaacs on 1/1/17.
+//  Copyright © 2017 Todd Isaacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TIImageViewer.
+FOUNDATION_EXPORT double TIImageViewerVersionNumber;
+
+//! Project version string for TIImageViewer.
+FOUNDATION_EXPORT const unsigned char TIImageViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TIImageViewer/PublicHeader.h>
+
+
